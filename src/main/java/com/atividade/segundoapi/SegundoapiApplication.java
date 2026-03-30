003f77a59bba87de902a88fe23cd3e296bf9cdd3
brunoovando <brunovando2014@gmail.com>
2026-03-30 12:17:06 -0300

    Commit inicial do projeto minhaSegundaAPI

---
+++ src/main/java/com/atividade/segundoapi/SegundoapiApplication.java
@@ -0,0 +1,13 @@
+package com.atividade.segundoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atividade/segundoapi/SegundoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atividade.segundoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
